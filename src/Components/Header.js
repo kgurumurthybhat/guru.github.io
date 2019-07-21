2019b8de84133ebdc2618be484e4787679d48fde
Guru <kgurumurthybhat@gmail.com>
2019-07-21 12:49:22 -0400

    resume updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,6 +8,7 @@ class Header extends Component {
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
+      var profilepic= "images/"+this.props.data.image;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -35,7 +36,8 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <img className="profile-pic"  src={profilepic} alt="Guru's Profile Pic"  width="175px"/>
+            <h3><span>{occupation}</span>. <br/>{description}.</h3>
             <hr />
             <ul className="social">
                {networks}
