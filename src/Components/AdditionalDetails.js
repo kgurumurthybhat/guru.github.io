36e3a9dd9af1b0a58d9f235433e49973ea1223fb
Guru <kgurumurthybhat@gmail.com>
2019-07-30 17:50:13 -0400

    remove outide google analytics

---
+++ src/Components/AdditionalDetails.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+
+class AdditionalDetails extends Component {
+   render() {
+      return (
+         <section id="additional">
+            < div align="center">
+               <h4>Additional Details</h4>
+            </div>
+
+
+         </section>
+      );
+   }
+}
+
+export default AdditionalDetails;
